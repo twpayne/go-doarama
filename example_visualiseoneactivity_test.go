aa73b0a7406de6dd8e7b7a0c55f0df6395a3ab3e
Tom Payne <twpayne@gmail.com>
2015-09-10 22:11:49 +0200

    Add example documentation

---
+++ example_visualiseoneactivity_test.go
@@ -0,0 +1,51 @@
+package doarama_test
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/twpayne/go-doarama"
+)
+
+func Example() (*doarama.Visualisation, error) {
+
+	// Create the client using anonymous authentication
+	apiKey := "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+	apiName := "Your API Name"
+	userId := "userid"
+	client := doarama.NewClient(doarama.API_URL, apiKey, apiName).Anonymous(userId)
+
+	// Open the GPS track
+	filename := "activity GPS filename (GPX or IGC)"
+	gpsTrack, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer gpsTrack.Close()
+
+	// Create the activity
+	activity, err := client.CreateActivity(filepath.Base(filename), gpsTrack)
+	if err != nil {
+		return nil, err
+	}
+	log.Print("ActivityId: %d", activity.Id)
+
+	// Set the activity info
+	if err := activity.SetInfo(&doarama.ActivityInfo{
+		TypeId: 29, // Fly - Paraglide
+	}); err != nil {
+		return nil, err
+	}
+
+	// Create the visualisation
+	activities := []*doarama.Activity{activity}
+	visualisation, err := client.CreateVisualisation(activities)
+	if err != nil {
+		return nil, err
+	}
+	log.Print("VisualisationKey: %s", visualisation.Key)
+	log.Print("VisualisationURL: %s", visualisation.URL(nil))
+
+	return visualisation, nil
+}
