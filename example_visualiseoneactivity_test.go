53276a8c5b2c48eb56cdac8e937f6de3c0640a2e
Tom Payne <twpayne@gmail.com>
2016-03-19 22:53:32 +0100

    Don't return anything in example code

--- example_visualiseoneactivity_test.go
+++ example_visualiseoneactivity_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/twpayne/go-doarama"
 )
 
-func Example() (*doarama.Visualisation, error) {
+func Example() {
 	// Create the client using anonymous authentication
 	apiKey := "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 	apiName := "Your API Name"
@@ -19,14 +19,14 @@ func Example() (*doarama.Visualisation, error) {
 	filename := "activity GPS filename (GPX or IGC)"
 	gpsTrack, err := os.Open(filename)
 	if err != nil {
-		return nil, err
+		return
 	}
 	defer gpsTrack.Close()
 
 	// Create the activity
 	activity, err := client.CreateActivity(filepath.Base(filename), gpsTrack)
 	if err != nil {
-		return nil, err
+		return
 	}
 	log.Printf("ActivityId: %d", activity.ID)
 
@@ -34,17 +34,15 @@ func Example() (*doarama.Visualisation, error) {
 	if err := activity.SetInfo(&doarama.ActivityInfo{
 		TypeID: doarama.FlyParaglide,
 	}); err != nil {
-		return nil, err
+		return
 	}
 
 	// Create the visualisation
 	activities := []*doarama.Activity{activity}
 	visualisation, err := client.CreateVisualisation(activities)
 	if err != nil {
-		return nil, err
+		return
 	}
 	log.Printf("VisualisationKey: %s", visualisation.Key)
 	log.Printf("VisualisationURL: %s", visualisation.URL(nil))
-
-	return visualisation, nil
 }
